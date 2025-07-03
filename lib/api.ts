bbd0f41b00d61a651e700dc5fcefaaed16cc8827
Evers D MBINI <eversdarrellmbini28@gmail.com>
2025-07-03 11:13:13 +0100

    connexion back-end

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@
 
 import { AuthService } from "./auth"
 
-const API_BASE_URL = "http://127.0.0.1:8000"
+const API_BASE_URL = "https://tasksmaster.onrender.com"
 
 export interface Master {
   id?: number
